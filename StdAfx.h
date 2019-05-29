f8cd40f1cc8928d2c6480a93d1befaa394540fe2
bright79 <mingliang79@aliyun.com>
2019-05-29 19:42:12 +0800

    Header file

--- StdAfx.h
+++ StdAfx.h
@@ -1,7 +1,7 @@
 // stdafx.h : include file for standard system include files,
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
-//
+////
 
 #if !defined(AFX_STDAFX_H__BF67B5C3_E953_4B7C_BBD3_ACF12AE072B4__INCLUDED_)
 #define AFX_STDAFX_H__BF67B5C3_E953_4B7C_BBD3_ACF12AE072B4__INCLUDED_
